2d4f2c1e54e83392573b77f68e49f92ad171f153
RomaGrek <loginovvv01@gmail.com>
2021-09-14 14:42:11 +0300

    chillO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,27 +12,75 @@ import java.util.*;
 для тестов:
 !A&!B->!(A|B)
 A->!B->!C&D|!T&U&V
+(A&B)&C
 */
 
 
 public class Main {
 
 
+    /* метод для проверки на то, что символ НЕ является операндом или скобкой  */
     public static boolean checkSymbols(String element) {
         return !element.equals("!") && !element.equals("&") && !element.equals("(") &&
                 !element.equals(")") && !element.equals("|") && !element.equals("->") &&
                 !element.equals("-") && !element.equals(">");
     }
 
+    /* метод для проверки на то, является ли это оператором или нет */
+    public static boolean checkOperator(String element) {
+        return element.equals("!") || element.equals("->") || element.equals("|") || element.equals("&");
+    }
+
+    /* метод получения индекса максмимального веса оператора,
+    * модифицируев выборкой макс веса, даже если несколько макимальных */
+    public static int indexOfMaxElement(ArrayList<Integer> list, ArrayList<String> listOperators) {
+        Integer[] array =  list.toArray(new Integer[list.size()]);
+        int indexMax = 0;
+        int countMaxElements = 0;
+        int maxElement = -1;
+        for (int i = 0; i < list.size(); i++) {
+            if (array[i] >= maxElement) {
+                maxElement = array[i];
+                indexMax = i;
+                countMaxElements++;
+            }
+        }
+
+
+
+        // что то тут не так...
+        if (countMaxElements > 1) {
+            String operator = listOperators.get(indexMax);
+            if (operator.equals("&") || operator.equals("|")) {
+                indexMax = 0;
+                maxElement = -1;
+                for (int i = 0; i < list.size(); i++) {
+                    if (array[i] > maxElement) {
+                        maxElement = array[i];
+                        indexMax = i;
+                    }
+                }
+            }
+        }
+        return indexMax;
+
+
+    }
+
+
     public static void main(String[] args) {
 
-
+        /* считываем строку */
         String inputLine;
         Scanner scanner = new Scanner(System.in);
         inputLine = scanner.nextLine();
 
         /* походу нужно сделать его листом, что бы можно было удалять элементы  */
         String[] stroke = inputLine.split("");  // 1 основной массив символов
+
+        /* соеднинеие элементов из нескольких символов */
+
+
 
 
         int count = 1;
@@ -77,9 +125,26 @@ public class Main {
             }
         }
 
-        /* процедура удаления нулей и скобок из основного списка */
+
+
+        /* для фикса бага с обработкой элементов состоящих изнескольких имен
+        * идея - соединять их тупо а текцщие обнулять
+        * кароче попробовать заново или другую идею фигачить */
+//        boolean flag = false;
+//        for (int i = 0; i < stroke.length; i++) {
+//            if (!checkOperator(stroke[i])) {
+//                if (!flag) flag = true ;
+//                else {
+//                    stroke[i - 1] += stroke[i];
+//                    stroke[i] = "0";
+//                }
+//            } else flag = false;
+//        }
+
+
 
         List<String> strokeList = new ArrayList<>(Arrays.asList(stroke));
+
 
         strokeList.removeIf(element -> element.equals("0"));
         strokeList.removeIf(element -> element.equals("("));
@@ -93,13 +158,57 @@ public class Main {
         * то есть смотрим их порядковый номер в массива operatorList и затем мы отсчитываем только операторы
         * в самом stroke используя доп счетчик и делаем выборку нужного оператора */
 
-//        stroke = strokeList.toArray(new String[strokeList.size()]);
+        stroke = strokeList.toArray(new String[strokeList.size()]);
+
+        int sumWeight = weightList.size();   // колчиество весов в начале
+        for (int i = 0; i < sumWeight; i++) {
+
+            // нужна проверка на то, что есть ли несколько операторов одного веса:
 
 
+            int maxIndex = indexOfMaxElement(weightList, operatorList); // инедкс наибольшего веса в массиве weightList
+
+            // ------------------------------------------------------------------- конец проверки на то, что есть нексолько операторов одного веса
+
+            String operator = operatorList.get(maxIndex);
+            
+            ////// ----- в четвертой итерации до этого момента все хорошо
+            int countOperatorNumber = -1;           // порялковый номер текцщего оператора в главном массиве
+            for (int j = 0; j < stroke.length; j++) {
+                if (checkOperator(stroke[j])) {
+                    countOperatorNumber++;
+                    if (stroke[j].equals(operator) && countOperatorNumber == maxIndex) {
+                        if (stroke[j].equals("!")) {
+                            stroke[j] = "(" + stroke[j] + stroke[j + 1] + ")";
+                            stroke[j + 1] = "0";
+                            System.out.print("обработка отрицания: ");
+                            System.out.println(stroke[j]);
+
+                        }else {
+                            System.out.print("обработка других опертаоров ");
+                            stroke[j] = "(" + stroke[j] + "," + stroke[j - 1] + "," + stroke[j + 1] + ")";
+                            stroke[j - 1] ="0";
+                            stroke[j + 1] = "0";
+                            System.out.println(stroke[j]);
+
+                        }
+                    }
+
+                }
+            }
+            // очищаем второстепенные массиы
+            weightList.remove(maxIndex);
+            operatorList.remove(maxIndex);
 
 
+            // нужно отчистить
+
+            List<String> strokeListo = new ArrayList<>(Arrays.asList(stroke));
+            strokeListo.removeIf(element -> element.equals("0"));
+            stroke = strokeListo.toArray(new String[0]);
 
 
+        }
 
 
 
@@ -120,11 +229,10 @@ public class Main {
 
 
         /* вывод основного массива stroke (там где мы будем в конце делать словарь) */
-        System.out.print("stroke: { ");
+        System.out.print("Ответ:  ");
         for (String s : stroke) {
             System.out.print(s + " ");
         }
-        System.out.println("} size: " + stroke.length);
 
 
 
